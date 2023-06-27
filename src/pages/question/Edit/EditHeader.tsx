f4ff1c5cdf407d7865865f0e3790fac013344a16
timmtiy <timmtiy@163.com>
2023-06-27 11:58:43 +0800

    feat: 保存问卷

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,17 +1,20 @@
 import React, { ChangeEvent, FC, useRef, useState } from 'react'
 import styles from './EditHeader.module.scss'
 import { Button, Input, Space, Typography } from 'antd'
-import { EditOutlined, LeftOutlined } from '@ant-design/icons'
-import { useNavigate } from 'react-router-dom'
+import { EditOutlined, LeftOutlined, LoadingOutlined } from '@ant-design/icons'
+import { useNavigate, useParams } from 'react-router-dom'
 import EditToolbar from './EditToolbar'
 import useGetPageInfo from '../../../hooks/useGetPageInfo'
 import type { InputRef } from 'antd'
 import { useDispatch } from 'react-redux'
 import { changePageTitle } from '../../../store/pageInfoReducer'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import { useKeyPress, useRequest } from 'ahooks'
+import { updateQuestionService } from '../../../service/question'
 
 const { Title } = Typography
 
-// 修改标题
+// 修改标题组件
 const TitleElem: FC = () => {
   const inputRef = useRef<InputRef>(null)
   const dispatch = useDispatch()
@@ -64,6 +67,57 @@ const TitleElem: FC = () => {
   }
 }
 
+// 保存按钮组件
+const SaveButton: FC = () => {
+  const { id } = useParams()
+  const { componentList } = useGetComponentInfo()
+  const pageInfo = useGetPageInfo()
+
+  const { loading, run: save } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionService(id, { ...pageInfo, componentList })
+    },
+    { manual: true }
+  )
+
+  // ctrl + s 快捷键保存
+  useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+    event.preventDefault()
+    if (!loading) save()
+  })
+
+  return (
+    <Button onClick={save} disabled={loading} icon={loading ? <LoadingOutlined /> : ''}>
+      保存
+    </Button>
+  )
+}
+
+// 发布按钮组件
+const PublishButton: FC = () => {
+  const { id } = useParams()
+
+  const { loading, run: publish } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionService(id, { isPublished: true })
+    },
+    { manual: true }
+  )
+
+  return (
+    <Button
+      type="primary"
+      onClick={publish}
+      disabled={loading}
+      icon={loading ? <LoadingOutlined /> : ''}
+    >
+      发布
+    </Button>
+  )
+}
+
 // 编辑器头部布局
 const EditHeader: FC = () => {
   const nav = useNavigate()
@@ -84,8 +138,8 @@ const EditHeader: FC = () => {
         </div>
         <div className={styles.right}>
           <Space>
-            <Button>保存</Button>
-            <Button type="primary">发布</Button>
+            <SaveButton />
+            <PublishButton />
           </Space>
         </div>
       </div>
