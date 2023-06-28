f1ff43af8412d87065058fbf92cb92ea02b452ad
timmtiy <timmtiy@163.com>
2023-06-28 17:13:06 +0800

    feat: 更改端口

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
-    port: 3000, // B 端，前端
+    port: 8000, // B 端，前端
     proxy: {
       // '/api': 'http://192.168.31.135:3001',
       '/api': 'http://localhost:3001', // mock
