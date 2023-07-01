2da33414091431a4e667708b999033eda0e20c72
timmtiy <timmtiy@163.com>
2023-07-01 12:59:37 +0800

    feat: 问卷统计 list

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,10 +3,14 @@
  * @Author: timmtiy
  * @Date: 2023-05-30 18:29:44
  * @LastEditors: timmtiy
- * @LastEditTime: 2023-06-02 11:51:48
+ * @LastEditTime: 2023-07-01 12:17:56
  */
 
 export const LIST_SEARCH_PARAM_KEY = 'keyword'
 export const LIST_PAGE_PARAM_KEY = 'page'
 export const LIST_PAGE_SIZE_PARAM_KEY = 'pageSize'
 export const LIST_PAGE_SIZE = 10
+
+export const STAT_PAGE_PARAM_KEY = 'page'
+export const STAT_PAGE_SIZE_PARAM_KEY = 'pageSize'
+export const STAT_PAGE_SIZE = 10
--- src/pages/question/Stat/index.tsx
+++ src/pages/question/Stat/index.tsx
@@ -7,6 +7,7 @@ import { useTitle } from 'ahooks'
 import styles from './index.module.scss'
 import StatHeader from './StatHeader'
 import ComponentList from './ComponentList'
+import PageStat from './PageStat'
 
 const Stat: FC = () => {
   const nav = useNavigate()
@@ -58,7 +59,13 @@ const Stat: FC = () => {
               />
             </div>
           </div>
-          <div className={styles.main}>main</div>
+          <div className={styles.main}>
+            <PageStat
+              selectedComponentId={selectedComponentId}
+              setSelectedComponentId={setSelectedComponentId}
+              setSelectedComponentType={setSelectedComponentType}
+            />
+          </div>
           <div className={styles.right}>right</div>
         </>
       )
